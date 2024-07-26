d6ca993c0969664d3fb282bce809d8a44fee11f1
vvmakarov <vvmakarov@corp.finam.ru>
2024-07-26 17:33:29 +0300

    0.0.40

--- src/main/suggest.ts
+++ src/main/suggest.ts
@@ -11,28 +11,38 @@ export const suggest = async (arg: {
   search: string,
   attributes?: string[],
   top?: number,
+  includeDisabled?: boolean,
 }) => {
   const client = new Client({ url: arg.url });
   const {
     top: sizeLimit = 0,
+    includeDisabled = false,
     attributes = [
       'sAMAccountName',
       'displayName',
       'userPrincipalName',
     ],
   } = arg;
+  attributes.push('userAccountControl');
 
   const search = (arg.search || '').trim().toLowerCase();
+  const onlyEnabledCond = includeDisabled ? '' : '(!(useraccountcontrol:1.2.840.113556.1.4.803:=2))';
 
   try {
     await client.bind(arg.username, arg.password);
     const options: SearchOptions = {
       scope: 'sub',
-      filter: `(&(objectCategory=person)(|(sAMAccountName=${search.toLowerCase().trim()}*)(name=${search.toLowerCase().trim()}*)))`,
+      filter: `(&${onlyEnabledCond}(objectCategory=person)(|(sAMAccountName=${search.toLowerCase().trim()}*)(name=${search.toLowerCase().trim()}*)))`,
       attributes,
       sizeLimit,
     };
     const { searchEntries } = await client.search(arg.baseDN, options);
+    searchEntries.map((item: any) => {
+      // eslint-disable-next-line no-bitwise
+      item.isDisabled = ((Number(item.userAccountControl) || 0) & 2) === 2;
+      return item;
+    });
+
     return searchEntries;
     // eslint-disable-next-line no-useless-catch
   } catch (err) {
@@ -41,5 +51,4 @@ export const suggest = async (arg: {
     await client.unbind();
   }
 };
-
-
+// !(useraccountcontrol:1.2.840.113556.1.4.803:=2)
